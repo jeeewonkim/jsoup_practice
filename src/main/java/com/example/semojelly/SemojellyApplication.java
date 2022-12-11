a6babc0af382bb7f51ec9608246d5db1326f7c61
jeon3 <wldnjs3865@naver.com>
2022-12-11 10:56:01 +0900

    jsoup practice

---
+++ src/main/java/com/example/semojelly/SemojellyApplication.java
@@ -0,0 +1,13 @@
+package com.example.semojelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemojellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemojellyApplication.class, args);
+	}
+
+}
